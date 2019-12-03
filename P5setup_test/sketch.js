55dc231f706a6512876f09b51301ceaab37fed33
rehafj <rj.codesstuff@gmail.com>
2019-12-02 19:54:29 -0800

    moved everything thank you javascript

--- P5setup_test/sketch.js
+++ P5setup_test/sketch.js
@@ -27,15 +27,15 @@ function setup(){
   createCanvas(1230,800);
 
 //make this into a funcion later
-  option1 = createButton('option1textinputfromfile').size(600,40);
-  option2 = createButton('option2PATHinputfromfile').size(600,40);
-  option3 = createButton('option33textinputfromfile').size(600,40);
-
-  option1.position(0, 700);
-  option2.position(0, 750);
-  option3.position(0, 790);
-
-  option1.mousePressed(doSomthing);
+  // option1 = createButton('option1textinputfromfile').size(600,40);
+  // option2 = createButton('option2PATHinputfromfile').size(600,40);
+  // option3 = createButton('option33textinputfromfile').size(600,40);
+  //
+  // option1.position(0, 700);
+  // option2.position(0, 750);
+  // option3.position(0, 790);
+  //
+  // option1.mousePressed(doSomthing);
 
   input = createInput();
   input.position(0, 0);
@@ -64,7 +64,6 @@ function keyPressed() {
 
   }
 }
-
   function greet(){
     print ("hello world!");
     let userInput= input.value();
@@ -74,7 +73,6 @@ function keyPressed() {
 
 
     }
-
 
 //bot functions
 
@@ -87,22 +85,10 @@ function chat() {
    //TODO if its not listed here have it go to one of the dge cases
 
    // show the reply
-   print(reply);
+   print(reply.promiseValue);
    // output.html(reply);
  }
-// function selfReply(message){
-//  // message_container.innerHTML += `<div class=”self”>${message}</div>`;
-//  // location.href = ‘#edge’;
-//  print(message);
-//  bot.reply(message).then(function(reply) {
-//  botReply(reply);
-//  });
-// }
-//
-// function botReply(message){
-//   print(message);
-//
-// }
+
 function botReady(){
    bot.sortReplies();
    print('does this happen? does the bot reply');//yes this *used to*  works
@@ -112,3 +98,90 @@ function botNotReady(err){
    print("An error has occurred",err);
 
     }
+
+FILE_DIR = "barDomain3";
+FILES = {"actions" : FILE_DIR + "/actions.json",
+		"cast" : FILE_DIR + "/cast.json",
+		"history" : FILE_DIR + "/history.json",
+		"schema" : FILE_DIR + "/schema.json",
+		"volitions"	: FILE_DIR + "/volitions.json",
+    "trigger"	: FILE_DIR + "/triggers.json"}
+
+document.addEventListener('ensembleLoaded', function (e){
+	console.log("Ensemble Loaded")
+	main();
+});
+
+function main() {
+	console.log(ensemble);
+	ensemble.init();
+
+	raw = {}
+
+	for (const [key, value] of Object.entries(FILES)) {
+	  raw[key] = ensemble.loadFile(value)
+
+	}
+
+	console.log(raw)
+
+	var schema = ensemble.loadSocialStructure(raw["schema"])
+	console.log(schema)
+
+	var rules = ensemble.addRules(raw["volitions"])
+	console.log(rules)
+
+	var actions = ensemble.addActions(raw["actions"])
+	console.log(actions)
+
+	var cast = ensemble.addCharacters(raw["cast"])
+	console.log(cast)
+
+	var history = ensemble.addHistory(raw["history"])
+	console.log(history)
+
+}
+
+function getAllVolitions() {
+	var cast = ensemble.getCharacters();
+	var calculatedVolitions = ensemble.calculateVolition(cast);
+
+	return calculatedVolitions
+}
+
+function getAllVolitionsFor(initiator) {
+	var allVolitions = getAllVolitions().dump();
+	return allVolitions[initiator]
+}
+
+function getActions(initiator, responder){
+	var cast = ensemble.getCharacters();
+	var calculatedVolitions = ensemble.calculateVolition(cast);
+	var actions = ensemble.getActions(initiator, responder, calculatedVolitions, cast, 100, 100, 100)
+
+	return actions;
+}
+
+function getAllActionsFor(initiator) {
+	var cast = ensemble.getCharacters();
+	var allActions = {}
+
+	for (var j = cast.length - 1; j >= 0; j--) {
+		try{
+			allActions[cast[j]] = getActions(initiator, cast[j]);
+		}catch(e){
+			allActions[cast[j]] = e;
+		}
+	}
+
+	return allActions
+}
+
+function getAllActions(){
+	var cast = ensemble.getCharacters();
+	var allActions = {}
+	for (var i = cast.length - 1; i >= 0; i--) {
+		allActions[cast[i]] = getAllActionsFor(cast[i])
+	}
+	return allActions
+}
